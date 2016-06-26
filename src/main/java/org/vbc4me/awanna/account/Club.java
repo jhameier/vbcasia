6bf7f1ef9c893a8e26642f9271823a693ab60059
John Hameier <jhameier@gmail.com>
2016-06-26 16:46:41 -0400

    Expanded the class to have an internal unmodifiable map to allow returning the enum via a standard string name.

--- src/main/java/org/vbc4me/awanna/account/Club.java
+++ src/main/java/org/vbc4me/awanna/account/Club.java
@@ -1,10 +1,59 @@
 package org.vbc4me.awanna.account;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.jdom2.IllegalAddException;
+
+/**
+ *  Contains the club names that students can belong to with in the Awana association.
+ */
 public enum Club {
-	PUGGLES,
-	CUBBIES,
-	SPARKS,
-	TANDT,
-	TREK,
-	JOURNEY;
+	PUGGLES("Puggles"),
+	CUBBIES("Cubbies"),
+	SPARKS("Sparks"),
+	TANDT("T&T"),
+	TREK("Trek"),
+	JOURNEY("Journey");
+	
+	private String name;
+	private static final Map<String, Club> CLUB_MAP;
+	
+	/**
+	 * Constructor which takes a string representation.
+	 */
+	Club(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 *  Convenience method for retrieving the name of the club.
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	/**
+	 * Constructor for populating the internal unmodifiable map to allow returning 
+	 * the {@link Club} enum by name.
+	 */
+	static {
+		Map<String, Club> map = new ConcurrentHashMap<>();
+		for(Club club : Club.values()) {
+			map.put(club.getName(), club);
+		}
+		CLUB_MAP = Collections.unmodifiableMap(map);
+	}
+	
+	/**
+	 * Returns the Enum associated with the string name passed in.
+	 * This will throw an {@link IllegalArgumentException} if the name passed 
+	 * in is not found in the internal mapping.
+	 */
+	public static Club get(String name) {
+		if(!CLUB_MAP.containsKey(name))
+			throw new IllegalAddException("The club name: " + name + " does not exist.");
+		return CLUB_MAP.get(name);
+	}
 }
