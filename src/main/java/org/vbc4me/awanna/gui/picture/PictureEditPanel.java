eb8a525498a29cc045f9f6f33a658326763d2c57
John Hameier <jhameier@gmail.com>
2016-07-10 23:14:27 -0400

    Change the way the panel is created so that we use the paint component of the JLabel using a custom JLabel class. This is a work in progress.

--- src/main/java/org/vbc4me/awanna/gui/picture/PictureEditPanel.java
+++ src/main/java/org/vbc4me/awanna/gui/picture/PictureEditPanel.java
@@ -1,13 +1,11 @@
 package org.vbc4me.awanna.gui.picture;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 
 import javax.swing.AbstractAction;
@@ -16,160 +14,170 @@ import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
-import javax.swing.JScrollPane;
+import javax.swing.border.LineBorder;
 
 /**
- * This represents a workable panel that allows manipulation of images. This panel primary purpose allows modifications to the 
- * {@link BufferedImage image} in order to get to a cropped image of the size 100 x 100. This panel does not permanently store
- * the image but has the methods necessary to create the thumbnail image. As a side effect this will return an 
- * {@link ImageContainer Image Container} that is used to store the information needed by this class to make future changes.
+ * This represents a workable panel that allows manipulation of images. This
+ * panels primary purpose allows modifications to the {@link BufferedImage image}
+ * in order to get to a cropped image of the size 100 x 100. This panel does not
+ * permanently store the image but has the methods necessary to create the
+ * thumbnail image. As a side effect this will return an {@link ImageContainer
+ * Image Container} that is used to store the information needed by this class
+ * to make future changes.
  */
-public class PictureEditPanel extends JDialog{
+public class PictureEditPanel extends JDialog {
 	
 	private static final long serialVersionUID = 2852491616246379626L;
 	private ImageContainer imageContainer;
 	private final JDialog dialog;
-	private Dimension dialogSize = new Dimension(0, 0);
-	private final JLabel label;
+	private final ImageLabel label;
 	private ImageIcon icon;
-	private double scale = 1.0;
-
+	private final Dimension DEFAULT_DIALOG_SIZE = new Dimension(600, 560);
+	private final int DEFAULT_BORDER_SIZE = 30;
+	private Dimension scaledImageDimension = new Dimension(0, 0);
+	
+	
+	private JPanel imagePanel;
+	
 	public PictureEditPanel(JPanel parent, ImageContainer container) {
 		this.imageContainer = container;
 		this.dialog = new JDialog();
-		dialog.setMinimumSize(new Dimension(650, 560));
+		dialog.setMinimumSize(DEFAULT_DIALOG_SIZE);
+		dialog.setPreferredSize(DEFAULT_DIALOG_SIZE);
+		
 		// Scale the image to a standard size
 		Dimension dim = container.getImageSize();
+		
 		double scale = 600 / dim.getWidth();
-		Dimension sDim = new Dimension(600, (int)(dim.height * scale));
+		Dimension sDim = new Dimension(600, (int) (dim.height * scale));
 		
 		// Setup Dialog
 		dialog.setTitle("Create Thumbnail");
 		dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		dialog.getContentPane().setLayout(new BorderLayout());
-		icon = new ImageIcon(imageContainer.getScaledImage(sDim));
 		
-		// Create button panel	
+		icon = new ImageIcon(imageContainer.getScaledImage(sDim));
+		label = new ImageLabel(icon);
+		
+		imagePanel = new JPanel();
+		imagePanel.setLayout(new BorderLayout());
+		imagePanel.add(label, BorderLayout.CENTER);
+		dialog.add(imagePanel, BorderLayout.CENTER);
+		// Create button panel
 		dialog.getContentPane().add(new EditPictureButtonPanel(new EditActionListener()), BorderLayout.NORTH);
 		
-		label = new JLabel(icon);
-//		JScrollPane scrollPane = new JScrollPane(label);
-//		dialog.getContentPane().add(scrollPane, BorderLayout.CENTER);
-		
-		label.addComponentListener(new ComponentAdapter() {
-			@Override
-			public void componentResized(ComponentEvent e) {
-				// this creates a circular reference after rotation of the image
-//				scaleImageByWindowSize(dialog.getSize());
-			}
-		});
-		dialog.getContentPane().add(label, BorderLayout.CENTER);
-		dialog.addMouseListener(new EditPanelMouseAdaptor());
-		
-		dialog.pack();
 		dialog.setVisible(true);
-		
-		
-		// Get Glass Panel to Display Crop Box
-		
 	}
 	
+	// FIXME Move this into the paint component section of the Image Label class
 	private void rotateImage(int theta) {
+		// Get a new rotated image from the container and create a new container
 		BufferedImage rotated = imageContainer.rotate(imageContainer.cloneImage(), theta);
 		BufferedImage thumbnail = ImageContainer.createThumbnail(rotated);
-		ImageContainer newContainer = new ImageContainer(rotated, thumbnail, new Dimension(0, 0));
-		this.imageContainer = newContainer;		
-		dialog.remove(label);
-
-		// Scale the image to a standard size
-		scale = 600.0 / rotated.getWidth();
-		Dimension sDim = new Dimension(600, (int)(rotated.getHeight() * scale));
+		this.imageContainer = new ImageContainer(rotated, thumbnail, new Dimension(0, 0));
 		
-		if(sDim.height > 800) {
+		// Scale the image based on a standard 600 x 800 image (3x4 aspect ratio)
+		double scale = 600.0 / rotated.getWidth();
+		Dimension sDim = new Dimension(600, (int) (rotated.getHeight() * scale));
+		
+		if (sDim.height > 800) {
 			scale = 800.0 / rotated.getHeight();
-			sDim = new Dimension((int)(rotated.getWidth() * scale), 800);
+			sDim = new Dimension((int) (rotated.getWidth() * scale), 800);
 		}
 		
 		icon = new ImageIcon(ImageContainer.getScaledImage(rotated, sDim));
 		label.setIcon(icon);
-		dialog.getContentPane().add(label, BorderLayout.CENTER);
-		dialog.pack();
+		
+		imagePanel.repaint();
 	}
 	
-	private void scaleImageByWindowSize(Dimension dim) {
-		dialog.remove(label);
-		double localScale = dim.getHeight()/ imageContainer.getImageSize().getHeight();
-		Dimension sDim = new Dimension((int)(imageContainer.getImageSize().getWidth() * localScale), 
-																			(int)(imageContainer.getImageSize().getHeight() * localScale));
-		icon = new ImageIcon(ImageContainer.getScaledImage(imageContainer.cloneImage(), sDim));
-		label.setIcon(icon);
-		dialog.getContentPane().add(label, BorderLayout.CENTER);
-		dialog.pack();
-	}
-	
-	// Class for use with automatic scaling  TODO DOES NOT WORK AT THIS TIME
+	/**
+	 * Custom label that allows automatic resizing if the {@link ImageIcon} when
+	 * the window this is attached to is resized.
+	 */
 	protected class ImageLabel extends JLabel {
+		private static final long serialVersionUID = 1827785467459231872L;
+		private int xPos = 20;
+		private int yPos = 20;
+		private int width = 100;
+		private int height = 100;
+		
 		public ImageLabel(ImageIcon icon) {
 			super(icon);
+			setBorder(new LineBorder(getBackground(), DEFAULT_BORDER_SIZE));
 		}
+		
+		public void moveHorizontal(int distance) {
+			// FIXME Add constraints for keeping this inside of border area
+			xPos = xPos + distance;
+			revalidate();
+		}
+		
+		public void moveVertical(int distance) {
+			// FIXME Add constraints for keeping this inside of border area
+			yPos = yPos + distance;
+			revalidate();
+		}
+		
 		protected void paintComponent(Graphics g) {
-			if(getIcon() != null) {
-				drawScaledImage(this);
+			super.paintComponent(g);
+			if (getIcon() != null) {
+				Dimension target = this.getSize();												// target containers size JLabel
+				Dimension original = imageContainer.getImageSize();		// gets the size of the original image
+				
+				// Automatically set the the width to the target width then scale height measurement
+				scaledImageDimension.width = target.width;
+				scaledImageDimension.height = (scaledImageDimension.width * original.height) / original.width;
+				
+				// Check to see if the height exceeds the current window height if so re-scale based off of height
+				if (scaledImageDimension.height > target.height) {
+					scaledImageDimension.height = target.height;
+					scaledImageDimension.width = (scaledImageDimension.height * original.width) / original.height;
+				}
+				
+				ImageIcon icon = new ImageIcon(
+						ImageContainer.getScaledImage(imageContainer.cloneImage(), scaledImageDimension));
+				setIcon(icon);
+
+				Graphics2D g2d = (Graphics2D)g.create();
+				g2d.setColor(Color.RED);
+				g2d.drawRect(xPos, yPos, width, height);
 			}
 		}
 	}
 	
-	/**
-	 *  Adapted from: http://www.codejava.net/java-se/graphics/drawing-an-image-with-automatic-scaling
-	 */
-	protected void drawScaledImage(ImageLabel label) {
-        int imageWidth = imageContainer.cloneImage().getWidth();
-        int imageHeight = imageContainer.cloneImage().getHeight();
-        double imageAspectRatio = (double) imageHeight / imageWidth;
- 
-        int labelWidth = label.getWidth();
-        int labelHeight = label.getHeight();
-        double labelAspectRatio = (double) labelHeight / labelWidth;
-        
-        // (x1, y1) ~ image top left corner
-        int x1 = 0; 
-        int y1 = 0; 
-        // (x2, y2) ~ image bottom right corner
-        int x2 = 0;
-        int y2 = 0;
-         
-        // Image smaller than container 
-        if (imageWidth < labelWidth && imageHeight < labelHeight) {
-            x1 = (labelWidth - imageWidth)  / 2;
-            y1 = (labelHeight - imageHeight) / 2;
-            x2 = x1 + imageWidth;
-            y2 = y1 + imageHeight;
-        } else { // image larger than container
-            if (labelAspectRatio > imageAspectRatio) {
-                y1 = labelHeight;
-                labelHeight = (int) (labelWidth * imageAspectRatio);
-                y1 = (y1 - labelHeight) / 2;
-            } else {
-                x1 = labelWidth;
-                labelWidth = (int) (labelHeight / imageAspectRatio);
-                x1 = (x1 - labelWidth) / 2;
-            }
-            x2 = x1 + labelWidth;
-            y2 = y1 + labelHeight;
-        }
-        label.getGraphics().drawImage(imageContainer.cloneImage(), x1, y1, x2, y2, 0, 0, imageWidth, imageHeight, null);
-    }
-	
-	private class EditPanelMouseAdaptor extends MouseAdapter {
-		public void mousePressed(MouseEvent e) {
-			// handle the mouse to move the crop box to create the offset
-			// the image displayed has to account for scaling of the image since the image is considered full size
+	private class ImagePanel extends JPanel {
+		private static final long serialVersionUID = 3218456572566697606L;
+		private int xPos = 20 + DEFAULT_BORDER_SIZE;
+		private int yPos = 20 + DEFAULT_BORDER_SIZE;
+		private int width = 100;
+		private int height = 100;
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			Graphics2D g2d = (Graphics2D)g.create();
+			g2d.setColor(Color.RED);
+			g2d.drawRect(xPos, yPos, width, height);
+			
 		}
+		
+		public void moveHorizontal(int distance) {
+			// FIXME Add constraints for keeping this inside of border area
+			xPos = xPos + distance;
+			revalidate();
+		}
+		
+		public void moveVertical(int distance) {
+			// FIXME Add constraints for keeping this inside of border area
+			yPos = yPos + distance;
+			revalidate();
+		}
+		
 	}
 	
 	protected class EditActionListener extends AbstractAction {
 		private static final long serialVersionUID = 3252488674763352416L;
 		
+		// Used when specifying the Action Commands used by buttons
 		String REDUCE = "reduce";
 		String EXPAND = "expand";
 		String CLOCKWISE = "clockwise";
@@ -180,15 +188,17 @@ public class PictureEditPanel extends JDialog{
 		String RIGHT = "right";
 		String SAVE = "save";
 		String CANCEL = "cancel";
-
+		
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			switch(e.getActionCommand()) {
+			switch (e.getActionCommand()) {
 				case "reduce":
 					// reduce the size of the crop box
+					
 					break;
 				case "expand":
 					// expand the size of the crop box
+
 					break;
 				case "clockwise":
 					// rotate the full image clockwise
@@ -200,27 +210,33 @@ public class PictureEditPanel extends JDialog{
 					break;
 				case "up":
 					// move the crop box up
+					label.moveVertical(-2);
 					break;
 				case "down":
 					// move the crop box down
+					label.moveVertical(2);
 					break;
 				case "left":
 					// move the crop box left
+					label.moveHorizontal(-2);
 					break;
 				case "right":
 					// move the crop box right
+					label.moveHorizontal(2);
 					break;
 				case "save":
-					// save the current image and thumbnail to new ImageContainer
+					// save the current image and thumbnail to new
+					// ImageContainer
+					System.out.println("Icon Dimensions : " + icon.getIconWidth() + ":" + icon.getIconHeight());
+					System.out.println("nDim (used when paiting label) : " + scaledImageDimension);
 					break;
 				case "cancel":
 					dialog.dispose();
 					break;
 				default:
-					throw new IllegalArgumentException("Unknown action command" + e.getActionCommand() );			
+					throw new IllegalArgumentException("Unknown action command" + e.getActionCommand());
 			}
 		}
-		
 	}
 	
 }
