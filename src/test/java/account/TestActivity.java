873bb7ded6adb7401e8c266677868d7ca275d64e
John Hameier <jhameier@gmail.com>
2016-08-07 00:07:34 -0400

    Remove unused import.

--- src/test/java/account/TestActivity.java
+++ src/test/java/account/TestActivity.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
