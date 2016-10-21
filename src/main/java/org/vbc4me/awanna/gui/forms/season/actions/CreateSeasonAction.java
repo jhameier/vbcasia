d555430f8b08ffe63026ba3b3585cbf838e15bab
John Hameier <jhameier@gmail.com>
2016-10-21 10:56:49 -0400

    basic implenetation for an action to create a new Season

---
+++ src/main/java/org/vbc4me/awanna/gui/forms/season/actions/CreateSeasonAction.java
@@ -0,0 +1,33 @@
+package org.vbc4me.awanna.gui.forms.season.actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JPanel;
+
+import org.vbc4me.awanna.gui.AppGui;
+import org.vbc4me.awanna.gui.forms.season.SeasonButtonPanel;
+import org.vbc4me.awanna.gui.forms.season.SeasonEditForm;
+import org.vbc4me.awanna.gui.forms.season.SeasonTableModel;
+
+/**
+ * Used to create a new Season with the name and date from the session panel.
+ *
+ * @author John Hameier: June 2015.
+ */
+@SuppressWarnings("unused")
+public class CreateSeasonAction extends AbstractAction {
+	private static final long serialVersionUID = 5370980765758188903L;
+	private JPanel panel;
+	
+	public CreateSeasonAction(JPanel panel) {
+		this.panel = panel;
+		putValue(NAME, "Create Season");
+		putValue(SHORT_DESCRIPTION, "Create a New Season.");
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+}
