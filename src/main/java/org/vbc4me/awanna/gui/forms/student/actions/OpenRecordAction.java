2fd4507226ad7f6edac3dd819e899ef7d376c168
John Hameier <jhameier@gmail.com>
2016-10-21 12:21:30 -0400

    moved from upper level actionsl directory and set to disabled by default

--- src/main/java/org/vbc4me/awanna/gui/forms/student/actions/OpenRecordAction.java
+++ src/main/java/org/vbc4me/awanna/gui/forms/student/actions/OpenRecordAction.java
@@ -1,4 +1,4 @@
-package org.vbc4me.awanna.gui.actions.record;
+package org.vbc4me.awanna.gui.forms.student.actions;
 
 import java.awt.event.ActionEvent;
 
@@ -8,23 +8,24 @@ import javax.swing.JOptionPane;
 
 /**
  * Used to create a new blank record.
- * 
+ *
  * @author John Hameier: June 2015.
  */
 public class OpenRecordAction extends AbstractAction {
     private static final long serialVersionUID = -6301577011454895115L;
     private JFrame frame;
-    
+
     public OpenRecordAction(JFrame frame) {
-	this.frame = frame;
-	putValue(NAME, "Open Record");
-	putValue(SHORT_DESCRIPTION, "Opens an existing Record.");
+        this.frame = frame;
+        putValue(NAME, "Open Record");
+        putValue(SHORT_DESCRIPTION, "Opens an existing Record.");
+        setEnabled(false);
     }
 
     public void actionPerformed(ActionEvent e) {
-	String message = "This is activated from Open Record Action";
-	String title = "Open Existing Record";
-	int messageType = JOptionPane.INFORMATION_MESSAGE;
-	JOptionPane.showMessageDialog(frame, message, title, messageType);
+        String message = "This is activated from Open Record Action";
+        String title = "Open Existing Record";
+        int messageType = JOptionPane.INFORMATION_MESSAGE;
+        JOptionPane.showMessageDialog(frame, message, title, messageType);
     }
 }
---
+++ src/main/java/org/vbc4me/awanna/gui/forms/student/actions/SaveRecordAction.java
@@ -0,0 +1,26 @@
+package org.vbc4me.awanna.gui.forms.student.actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class SaveRecordAction extends AbstractAction {
+    private static final long serialVersionUID = -1288201101615570596L;
+    private JPanel panel;
+
+    public SaveRecordAction(JPanel panel) {
+        this.panel = panel;
+        putValue(NAME, "Save Record");
+        putValue(SHORT_DESCRIPTION, "Saves the currently loaded Record");
+        setEnabled(false);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String message = "This is activated from Save Record Action";
+        String title = "Save Record";
+        int messageType = JOptionPane.INFORMATION_MESSAGE;
+        JOptionPane.showMessageDialog(panel, message, title, messageType);
+    }
+}
