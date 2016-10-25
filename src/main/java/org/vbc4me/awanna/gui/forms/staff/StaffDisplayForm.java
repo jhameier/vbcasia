4b9d2f1b239c9eac454d589c6fd096f12a76f858
John Hameier <jhameier@gmail.com>
2016-10-24 21:02:55 -0400

    place holders for staff

---
+++ src/main/java/org/vbc4me/awanna/gui/forms/staff/StaffDisplayForm.java
@@ -0,0 +1,11 @@
+package org.vbc4me.awanna.gui.forms.staff;
+
+import javax.swing.JPanel;
+
+public class StaffDisplayForm extends JPanel {
+	
+	public StaffDisplayForm() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ src/main/java/org/vbc4me/awanna/gui/forms/staff/activities/NewStaffRecordActivity.java
@@ -0,0 +1,21 @@
+package org.vbc4me.awanna.gui.forms.staff.activities;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+public class NewStaffRecordActivity extends AbstractAction{
+
+	private static final long serialVersionUID = 6052353184094817955L;
+
+	public NewStaffRecordActivity() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+	
+}
