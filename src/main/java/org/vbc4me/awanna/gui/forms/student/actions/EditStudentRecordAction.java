1995de5a8390c346ca386ede50ef81e6f48216fd
John Hameier <jhameier@gmail.com>
2017-08-26 08:15:30 -0400

    Local commit update to fix git sync problem

--- src/main/java/org/vbc4me/awanna/gui/forms/student/actions/EditStudentRecordAction.java
+++ src/main/java/org/vbc4me/awanna/gui/forms/student/actions/EditStudentRecordAction.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package org.vbc4me.awanna.gui.forms.student.actions;
 
 import java.awt.event.ActionEvent;
@@ -27,3 +28,34 @@ public final class EditStudentRecordAction extends AbstractAction {
         JOptionPane.showMessageDialog(panel, message, title, messageType);
     }
 }
+=======
+package org.vbc4me.awanna.gui.forms.student.actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+/**
+ * Action Class for Buttons and Menu Items
+ */
+public final class EditRecordAction extends AbstractAction {
+    private static final long serialVersionUID = -6301577011454895115L;
+    private JPanel panel;
+
+    public EditRecordAction(JPanel panel) {
+        this.panel = panel;
+        putValue(NAME, "Edit Record");
+        putValue(SHORT_DESCRIPTION, "Edits the currently loaded record information ");
+        setEnabled(false);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String message = "This is activated from Edit Record Action";
+        String title = "Edit Record";
+        int messageType = JOptionPane.INFORMATION_MESSAGE;
+        JOptionPane.showMessageDialog(panel, message, title, messageType);
+    }
+}
+>>>>>>> branch 'master' of https://github.com/jhameier/vbcasia.git
--- src/main/java/org/vbc4me/awanna/gui/forms/student/actions/SaveStudentRecordAction.java
+++ src/main/java/org/vbc4me/awanna/gui/forms/student/actions/SaveStudentRecordAction.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package org.vbc4me.awanna.gui.forms.student.actions;
 
 import java.awt.event.ActionEvent;
@@ -23,4 +24,31 @@ public final class SaveStudentRecordAction extends AbstractAction {
         int messageType = JOptionPane.INFORMATION_MESSAGE;
         JOptionPane.showMessageDialog(panel, message, title, messageType);
     }
+=======
+package org.vbc4me.awanna.gui.forms.student.actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public final class SaveRecordAction extends AbstractAction {
+    private static final long serialVersionUID = -1288201101615570596L;
+    private JPanel panel;
+
+    public SaveRecordAction(JPanel panel) {
+        this.panel = panel;
+        putValue(NAME, "Save Record");
+        putValue(SHORT_DESCRIPTION, "Saves the currently loaded Record");
+        setEnabled(false);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String message = "This is activated from Save Record Action";
+        String title = "Save Record";
+        int messageType = JOptionPane.INFORMATION_MESSAGE;
+        JOptionPane.showMessageDialog(panel, message, title, messageType);
+    }
+>>>>>>> branch 'master' of https://github.com/jhameier/vbcasia.git
 }
