8402d6392c38e5861b4fa3d1a6fd5017e39e6fec
John Hameier <jhameier@gmail.com>
2017-08-26 08:25:23 -0400

    Merge branch 'master' of https://github.com/jhameier/vbcasia.git
    Conflicts:
    	src/main/java/org/vbc4me/awanna/gui/forms/student/actions/CopyStudentRecordAction.java
    	src/main/java/org/vbc4me/awanna/gui/forms/student/actions/EditStudentRecordAction.java
    	src/main/java/org/vbc4me/awanna/gui/forms/student/actions/OpenStudentRecordAction.java
    	src/main/java/org/vbc4me/awanna/gui/forms/student/actions/SaveStudentRecordAction.java

--- src/main/java/org/vbc4me/awanna/gui/forms/student/actions/OpenStudentRecordAction.java
+++ src/main/java/org/vbc4me/awanna/gui/forms/student/actions/OpenStudentRecordAction.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.vbc4me.awanna.gui.forms.student.actions;
 
 import java.awt.event.ActionEvent;
@@ -29,36 +28,4 @@ public final class OpenStudentRecordAction extends AbstractAction {
         int messageType = JOptionPane.INFORMATION_MESSAGE;
         JOptionPane.showMessageDialog(frame, message, title, messageType);
     }
-=======
-package org.vbc4me.awanna.gui.forms.student.actions;
-
-import java.awt.event.ActionEvent;
-
-import javax.swing.AbstractAction;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-
-/**
- * Used to create a new blank record.
- *
- * @author John Hameier: June 2015.
- */
-public final class OpenRecordAction extends AbstractAction {
-    private static final long serialVersionUID = -6301577011454895115L;
-    private JFrame frame;
-
-    public OpenRecordAction(JFrame frame) {
-        this.frame = frame;
-        putValue(NAME, "Open Record");
-        putValue(SHORT_DESCRIPTION, "Opens an existing Record.");
-        setEnabled(false);
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        String message = "This is activated from Open Record Action";
-        String title = "Open Existing Record";
-        int messageType = JOptionPane.INFORMATION_MESSAGE;
-        JOptionPane.showMessageDialog(frame, message, title, messageType);
-    }
->>>>>>> branch 'master' of https://github.com/jhameier/vbcasia.git
 }
