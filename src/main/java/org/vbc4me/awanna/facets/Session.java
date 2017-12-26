ba4b2ce097853301b2cc568f1f79e935ca438546
John Hameier <jhameier@gmail.com>
2017-12-26 16:00:54 -0500

    Add the builder

--- src/main/java/org/vbc4me/awanna/facets/Session.java
+++ src/main/java/org/vbc4me/awanna/facets/Session.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.Objects;
+
 
 /**
  * Represents a logical amount of time including a set of activities that will
@@ -17,36 +19,22 @@ public final class Session {
 	private UUID id;
 	private LocalDate startDate;
 	private LocalDate endDate;
-	private Map<LocalDate, Activity> activities = new HashMap<>();
-	private boolean open = true;
+	private Map<LocalDate, Activity> activities;
+    /**
+     * Returns a {@link Builder} for {@code Session}s.
+     */
+	public static Builder builder() {
+	    return new Builder();
+    }
 
-	
-	/**
-	 * Creates a new Session with the given start date.
-	 * The end date is calculated based on the number of weeks passed in.
-	 * The end date can be adjusted when necessary. 
-	 * 
-	 * <p>The new session will have no activities associated with it and these must be added.
-	 */
-	public Session(LocalDate date, int numOfWeeks) {
-        this.id = UUID.randomUUID();
-		this.startDate = date;
-		this.endDate = date.plusWeeks(numOfWeeks);
-	}
+    private Session(Builder builder) {
+	    this.id = builder.id;
+	    this.startDate = builder.startDate;
+	    this.endDate = builder.endDate;
+	    this.activities = builder.activities;
+    }
 
     /**
-     * Creates a new Session with the given start and end dates.
-     * The end date can be adjusted when necessary.
-     *
-     * <p>The new session will have no activities associated with it and these must be added.
-     */
-	public Session(LocalDate start, LocalDate end) {
-        this.id = UUID.randomUUID();
-        this.startDate = start;
-        this.endDate = end;
-    }
-	
-	/**
 	 * Return the unique id for this {@link Session}.
 	 */
 	public UUID id() {
@@ -57,7 +45,6 @@ public final class Session {
 	 * Sets the end date of this Session to a specific date.
 	 */
 	public void endDate(LocalDate date) {
-		checkIfSessionIsOpen();
 		this.endDate = date;
 	}
 	
@@ -69,7 +56,6 @@ public final class Session {
 	 * @param unit  of measure to use in the operation
 	 */
 	public void moveEndDate(Long amount, ChronoUnit unit) {
-		checkIfSessionIsOpen();
 		if(amount < 0) {
 			this.endDate.minus(amount * -1, unit);
 			return;
@@ -92,22 +78,18 @@ public final class Session {
 	}
 	
 	/**
-	 * Adds an actions to this Session
-	 *
-	 * @param date  of this actions
-	 * @param activity for members to participate in
+	 * Adds an {@link Activity} to this {@code Session}.
 	 */
-	public void addActivity(LocalDate date, LocalTime time, String activity, double cost) {
-		checkIfSessionIsOpen();
-		Activity act = Activity.builder().date(date).time(time).name(activity).cost(cost).create();
-		activities.put(act.date(), act);
+	public void addActivity(LocalDate date, LocalTime time, String name, double cost) {
+		activities.put(date, Activity.builder().date(date).time(time).name(name).cost(cost).create());
 	}
 	
 	/**
-	 *  Returns an actions if one is found on the date specified. If not an empty object will be returned
+	 *  Returns an {@link Optional} {@link Activity} if one is found on the date specified else returns an
+     *  {@link Optional#empty()}.
 	 */
 	public Optional<Activity> getActivity(LocalDate date) {
-		if (activities.get(date) != null) {
+		if (activities.containsKey(date)) {
 			return Optional.of(activities.get(date));
 		}
 		return Optional.empty();
@@ -119,45 +101,61 @@ public final class Session {
 	public Map<LocalDate, Activity> activities() {
 		return activities;
 	}
-	
-	/**
-	 * Checks if this actions is still open. This will return nothing if this
-	 * actions is open but conversely will throw an {@link IllegalAccessError}
-	 * if it has been closed. This is here in an attempt to keep someone from performing
-	 * operations on an already closed actions and it is left to the programmer to control
-	 * the program flow in order to avoid this from being called after a actions has been closed.
-	 * the {@link #sessionIsOpen()} method should be called in order to avoid this from being
-	 * thrown.
-	 */
-	private void checkIfSessionIsOpen() {
-		if (!open) {
-			throw new IllegalAccessError("You can not add an actions to a closed actions");
-		}
-	}
-	
-	/**
-	 * Returns if this actions is open and available to add and modify students,
-	 * staff and activities.
-	 */
-	public boolean sessionIsOpen() {
-		return open;
-	}
-	
-	/**
-	 * Closes this actions. Closed sessions can not be modified in anyway after
-	 * it has been closed.
-	 */
-	public void closeSession() {
-		open = false;
-	}
-	
-	/**
-	 * This will reopen a actions if it is closed. This should only be used in
-	 * rare circumstances that require addition information to be added. Add
-	 * data in an open actions is logged for backup purposes.
-	 */
-	protected void reopenSession() {
-		open = true;
-	}
-	
+
+    /**
+     * A builder to help in the instantiation of a new {@link Session}.
+     */
+	public static class Builder {
+        private UUID id;
+        private LocalDate startDate;
+        private LocalDate endDate;
+        private Map<LocalDate, Activity> activities = new HashMap<>();
+
+        /**
+         * The {@link UUID} to associate with this {@link Session}.  Returns this builder for method chaining.
+         */
+        public Builder id(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        /**
+         * The {@link LocalDate starting date} to associate with this {@link Session}.   Returns this builder for
+         * method chaining.
+         */
+        public Builder startDate(LocalDate date) {
+            this.startDate = date;
+            return this;
+        }
+
+        /**
+         * The {@link LocalDate ending date} to asscoicate with this {@link Session}.    Returns this builder for
+         * method chaining.
+         */
+        public Builder endDate(LocalDate date) {
+            this.endDate = date;
+            return this;
+        }
+
+        /**
+         * Adds an {@link Activity} to the list of Activities associated with this {@link Session}.  Returns this
+         * builder for method chaining.
+         */
+        public Builder activity(Activity activity) {
+            this.activities.put(activity.date(), activity);
+            return this;
+        }
+
+        /**
+         * Returns an new fully qualified {@link Session}.
+         */
+        public Session build() {
+            if (id == null) {
+                id = UUID.randomUUID();
+            }
+            Objects.requireNonNull(startDate);
+            Objects.requireNonNull(endDate);
+            return new Session(this);
+        }
+    }
 }
