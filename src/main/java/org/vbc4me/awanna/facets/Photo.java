4106afde8b7a07f3a165b29f6e352948b8fd26be
John Hameier <jhameier@gmail.com>
2017-12-26 15:54:46 -0500

    Simplify object storing only the images themselves. We can now write the images out to an xml file directly so no need to store the photos local anymore.

--- src/main/java/org/vbc4me/awanna/facets/Photo.java
+++ src/main/java/org/vbc4me/awanna/facets/Photo.java
@@ -8,41 +8,17 @@ import java.util.Objects;
  * Structure to hold photo, thumbnail and path to the image file.
  */
 public class Photo {
-	private final Path photoPath;
 	private final BufferedImage photo;
-	private final Path thumbPath;
 	private final BufferedImage thumbnail;
 	
 	/**
 	 * Create an immutable structure to hold images.
-	 * @param path: the path to the image
-	 * @param photo: the image 
-	 * @param tpath: the path to the thumbnail image
-	 * @param thumbnail: the thumbnail of the image
+	 * @param photo: the image
+	 * @param thumb: the thumbnail of the image
 	 */
-	public Photo(Path path, BufferedImage image, Path tpath, BufferedImage thumb) {
-		Objects.requireNonNull(path);
-		Objects.requireNonNull(image);
-		Objects.requireNonNull(tpath);
-		Objects.requireNonNull(thumb);
-		this.photoPath = path;
-		this.photo = image;
-		this.thumbPath = tpath;
-		this.thumbnail = thumb;
-	}
-
-	/**
-	 * @return the path
-	 */
-	public Path photoPath() {
-		return photoPath;
-	}
-	
-	/**
-	 * @return the thumbnail path
-	 */
-	public Path thumbnailPath() {
-		return thumbPath;
+	public Photo(BufferedImage photo, BufferedImage thumb) {
+		this.photo = Objects.requireNonNull(photo);
+		this.thumbnail = Objects.requireNonNull(thumb);
 	}
 	
 	/**
