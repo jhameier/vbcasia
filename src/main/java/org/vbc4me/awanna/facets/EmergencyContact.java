e170e6bc6ccc5eb7541e44f8ce1af7f1305fecfc
John Hameier <jhameier@gmail.com>
2017-12-26 20:23:44 -0500

    New Type for the emergency contact to replace the single use name and phone number.

---
+++ src/main/java/org/vbc4me/awanna/facets/EmergencyContact.java
@@ -0,0 +1,69 @@
+package org.vbc4me.awanna.facets;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class EmergencyContact extends Person {
+
+    private final PhoneNumber phoneNumber;
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    private EmergencyContact(Builder builder) {
+        super(builder.id, builder.firstName, builder.lastName, builder.type);
+        this.phoneNumber = builder.phoneNumber;
+    }
+
+    /**
+     * Return the {@link PhoneNumber} associated with this {@link EmergencyContact}.
+     */
+    public PhoneNumber phoneNumber() {
+        return phoneNumber;
+    }
+
+    public static class Builder {
+        private UUID id;
+        private String firstName;
+        private String lastName;
+        private Type type;
+        private PhoneNumber phoneNumber;
+
+        public Builder id(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder type(Type type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder phoneNumber(PhoneNumber phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public EmergencyContact create() {
+            if (id == null) {
+                id = UUID.randomUUID();
+            }
+            Objects.requireNonNull(firstName);
+            Objects.requireNonNull(lastName);
+            Objects.requireNonNull(type);
+            Objects.requireNonNull(phoneNumber);
+            return new EmergencyContact(this);
+        }
+    }
+}
