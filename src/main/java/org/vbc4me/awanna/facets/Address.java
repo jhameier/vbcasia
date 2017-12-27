9a7ddfea1d845bf4fe7830a8d2d0fab77eede97c
John Hameier <jhameier@gmail.com>
2017-12-26 20:43:51 -0500

    New Type for Address to replace the single use address/city/state/zip

---
+++ src/main/java/org/vbc4me/awanna/facets/Address.java
@@ -0,0 +1,69 @@
+package org.vbc4me.awanna.facets;
+
+import java.util.Objects;
+
+public class Address {
+    private final String streetAddress;
+    private final String city;
+    private final String state;
+    private final Zipcode zipcode;
+
+    private Address(Builder builder) {
+        this.streetAddress = builder.streetAddress;
+        this.city = builder.city;
+        this.state = builder.state;
+        this.zipcode = builder.zipcode;
+    }
+
+    public String streetAddress() {
+        return streetAddress;
+    }
+
+    public String city() {
+        return city;
+    }
+
+    public String state() {
+        return state;
+    }
+
+    public Zipcode zipcode() {
+        return zipcode;
+    }
+
+    public static class Builder {
+
+        private String streetAddress;
+        private String city;
+        private String state;
+        private Zipcode zipcode;
+
+        public Builder streetAddress(String streetAddress) {
+            this.streetAddress = streetAddress;
+            return this;
+        }
+
+        public Builder city(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Builder state(String state) {
+            this.state = state;
+            return this;
+        }
+
+        public Builder zipcode(Zipcode zipcode) {
+            this.zipcode = zipcode;
+            return this;
+        }
+
+        public Address create() {
+            Objects.requireNonNull(streetAddress);
+            Objects.requireNonNull(city);
+            Objects.requireNonNull(state);
+            Objects.requireNonNull(zipcode);
+            return new Address(this);
+        }
+    }
+}
