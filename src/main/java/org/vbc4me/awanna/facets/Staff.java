0178edd80451c9dbfe73337ae50bc992d8af5e89
John Hameier <jhameier@gmail.com>
2017-12-26 21:23:51 -0500

    Update class to use new constructs such as Address and Emergency Contact to make it easier to read and write from a flat file.

--- src/main/java/org/vbc4me/awanna/facets/Staff.java
+++ src/main/java/org/vbc4me/awanna/facets/Staff.java
@@ -1,32 +1,22 @@
 package org.vbc4me.awanna.facets;
 
 import java.awt.image.BufferedImage;
-import java.io.FileNotFoundException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
  * A staff is a individual of the organization that helps to oversee some event.
  */
 public class Staff extends Person{
-	private UUID id;
-	private String address;
-	private String city;
-	private String state;
-	private String zip;
+    private final Address address;
 	private List<PhoneNumber> phoneNumbers;
 	private String email;
 	private BufferedImage photo;
-	private Path photoPath;
 	private BufferedImage thumbnail;
-	private Path thumbnailPath;
 	private String specialNeeds = "";
-	private String emergencyContactName;
-	private PhoneNumber emergencyContactPhone;
+	private EmergencyContact emergencyContact;
 	private Club assignedClub;
 	private String title;
 	
@@ -36,13 +26,37 @@ public class Staff extends Person{
 	public static Builder builder() {
 		return new Builder();
 	}
-	
+
+    /**
+     * Constructs a new {@link Staff} object.
+     */
+    private Staff(Builder builder) {
+        super(builder.id, builder.firstName, builder.lastName, Person.Type.STAFF);
+        this.title = builder.title;
+        this.assignedClub = builder.club;
+        this.address = builder.address;
+        this.phoneNumbers = builder.phoneNumbers;
+        this.specialNeeds = builder.specialNeeds;
+        this.email = builder.email;
+        this.photo = builder.photo;
+        this.thumbnail = builder.thumbnail;
+        this.emergencyContact = builder.emergencyContact;
+
+    }
+
 	/**
 	 * Returns the record childId
 	 */
-	public String id() {
-		return id.toString();
+	public String staffId() {
+		return id().toString();
 	}
+
+    /**
+     * Return the {@link Address} for this {@link Staff} member.
+     */
+	public Address address() {
+	    return address;
+    }
 	
 	/**
 	 *  Sets the title of this {@link Staff} member.
@@ -66,71 +80,6 @@ public class Staff extends Person{
 	}
 	
 	/**
-	 * Returns the staff members address of residence
-	 */
-	public String address() {
-		return address;
-	}
-	
-	/**
-	 * Allows for input or updating a staff members address of residence.
-	 * 
-	 * @param address
-	 *            to update to
-	 */
-	public void address(String address) {
-		this.address = address;
-	}
-	
-	/**
-	 * Returns the staff members city of residence
-	 */
-	public String city() {
-		return city;
-	}
-	
-	/**
-	 * Allows for input or update of the staff members city of residence.
-	 * 
-	 * @param city
-	 *            of residence
-	 */
-	public void city(String city) {
-		this.city = city;
-	}
-	
-	/**
-	 * Returns the staff members state of residence
-	 */
-	public String state() {
-		return state;
-	}
-	
-	/**
-	 * Allows for input or update of the staff members state of residence.
-	 * 
-	 * @param state
-	 *            of residence
-	 */
-	public void state(String state) {
-		this.state = state;
-	}
-	
-	/**
-	 * Returns the staff members zip
-	 */
-	public String zip() {
-		return zip;
-	}
-	
-	/**
-	 * Sets the zip code of this {@link Staff}  member.
-	 */
-	public void zip(String zipcode) {
-		this.zip = zipcode;
-	}
-	
-	/**
 	 * Returns the staff members list of phone numbers
 	 */
 	public List<PhoneNumber> phoneNumbers() {
@@ -142,67 +91,18 @@ public class Staff extends Person{
 	 * number should be in its raw form without formatting characters. If any
 	 * formatting characters are present or the input is not 10 characters in
 	 * length the creation method will throw an Illegal Argument exception.
-	 *
-	 * @param type
-	 *            of phone number such as home or cell etc.
-	 * @param number
-	 *            to add
 	 */
-	public void addUnformattedPhoneNumber(String type, String number) {
-		PhoneNumber ph = new PhoneNumber(type, number);
-		this.phoneNumbers.add(ph);
+	public void addUnformattedPhoneNumber(PhoneNumber number) {
+		this.phoneNumbers.add(Objects.requireNonNull(number));
 	}
-	
-	/**
-	 * Removes a phone number from this staff memeber's list of numbers. The
-	 * number can be formatted xxx-xxx-xxxx or (xxx) xxx-xxxx or unformatted
-	 * xxxxxxxxxx and this method call will throw and Illegal Argument Exception
-	 * if the number is not found in the list of known phone numbers.
-	 *
-	 * @param number
-	 *            to be removed from this staff memebers list of phone numbers
-	 */
-	public void removePhoneNumber(String number) {
-		number = number.replace("(", "");
-		number = number.replace(")", "");
-		number = number.replace("-", "");
-		number = number.replace(" ", "");
-		
-		PhoneNumber pnumber = null;
-		for (PhoneNumber ph : this.phoneNumbers) {
-			if (ph.number(false).equals(number)) {
-				pnumber = ph;
-				break;
-			}
-		}
-		if (pnumber == null) {
-			throw new IllegalArgumentException("Phone number not found in list");
-		}
-		this.phoneNumbers.remove(pnumber);
-	}
-	
-	public String emergencyContactName() {
-		return emergencyContactName;
-	}
-	
-	public void emergencyContactName(String name) {
-		this.emergencyContactName = name;
-	}
-	
-	/**
-	 * Returns the emergency contact number assigned to this members record.
-	 */
-	public PhoneNumber emergencyContactNumber() {
-		return emergencyContactPhone;
-	}
-	
-	/**
-	 * Assign an emergency contact phone number
-	 */
-	public void emergencyContactName(PhoneNumber number) {
-		this.emergencyContactPhone = number;
-	}
-	
+
+    /**
+     * Return the {@link EmergencyContact} associated with this {@link Staff} memeber.
+     */
+    public EmergencyContact emergencyContact() {
+	    return emergencyContact;
+    }
+
 	/**
 	 * Returns the staff members special needs
 	 */
@@ -231,8 +131,7 @@ public class Staff extends Person{
 	/**
 	 * Adds an email address to this staff members record.
 	 *
-	 * @param email
-	 *            address to add
+	 * @param email address to add
 	 */
 	public void email(String email) {
 		this.email = email;
@@ -246,27 +145,10 @@ public class Staff extends Person{
 	}
 	
 	/**
-	 * Returns the path to the image file of this staff member. 
-	 */
-	public Path photoPath() {
-		return photoPath;
-	}
-	
-	/**
 	 * Adds a photo to this staff members record.
 	 */
 	public void photo(BufferedImage image) {
 		this.photo = image;
-	}
-	
-	/**
-	 * Adds a path to this staffs member image file;
-	 */
-	public void photoPath(String filepath) throws FileNotFoundException {
-		Path path = Paths.get(filepath);
-		if(!Files.exists(path))
-			throw new FileNotFoundException("The file " + path + " does not exist.");
-		this.photoPath = path;
 	}
 	
 	/**
@@ -275,44 +157,11 @@ public class Staff extends Person{
 	public BufferedImage thumbnail() {
 		return thumbnail;
 	}
-	
-	/**
-	 * Returns the path to the image file of this staff member. 
-	 */
-	public Path thumbanilPath() {
-		return thumbnailPath;
-	}
-	
 	/**
 	 * Adds a thumbnail photo of this staff member.
 	 */
 	public void thumbnail(BufferedImage image) {
 		this.thumbnail = image;
-	}
-	
-	/**
-	 * Adds a path to the thumbnail image file.
-	 */
-	
-	/**
-	 * Constructs a new {@link Staff} object.
-	 */
-	private Staff(Builder builder) {
-		super(builder.firstName, builder.lastName, Person.Type.STAFF);
-		this.title = builder.title;
-		this.assignedClub = builder.club;
-		this.address = builder.address;
-		this.city = builder.city;
-		this.state = builder.state;
-		this.zip = builder.zip;
-		this.phoneNumbers = builder.phoneNumbers;
-		this.specialNeeds = builder.specialNeeds;
-		this.email = builder.email;
-		this.photo = builder.photo;
-		this.thumbnail = builder.thumbnail;
-		this.emergencyContactName = builder.emerContactName;
-		this.emergencyContactPhone = builder.emerContactNumber;
-		this.id = UUID.randomUUID();
 	}
 	
 	public static class Builder {
@@ -321,17 +170,24 @@ public class Staff extends Person{
 		private Club club;
 		private String firstName;
 		private String lastName;
-		private String address;
+
+		private String streetAddress;
 		private String city;
 		private String state;
-		private String zip;
+		private Zipcode zip;
+		private Address address;
+
 		private List<PhoneNumber> phoneNumbers = new ArrayList<>();
-		private String specialNeeds;
+		private String specialNeeds = "";
 		private String email;
 		private BufferedImage photo;
 		private BufferedImage thumbnail;
-		private String emerContactName;
-		private PhoneNumber emerContactNumber;
+
+		private UUID emerId;
+		private String emerFirstName;
+		private String emerLastName;
+		private PhoneNumber emerPhoneNumber;
+		private EmergencyContact emergencyContact;
 		
 		public Builder id(UUID id) {
 		    this.id = id;
@@ -359,7 +215,7 @@ public class Staff extends Person{
 		}
 		
 		public Builder address(String address) {
-			this.address = address;
+			this.streetAddress = address;
 			return this;
 		}
 		
@@ -374,15 +230,24 @@ public class Staff extends Person{
 		}
 		
 		public Builder zip(String zip) {
-			this.zip = zip;
+			this.zip = new Zipcode(zip);
 			return this;
 		}
+
+		public Builder address(Address address) {
+		    this.address = address;
+		    return this;
+        }
 		
 		public Builder phoneNumber(String type, String number) {
-			PhoneNumber ph = new PhoneNumber(type, number);
-			this.phoneNumbers.add(ph);
-			return this;
+			PhoneNumber ph = new PhoneNumber(PhoneNumber.Type.valueOf(type.toUpperCase()), number);
+			return phoneNumber(ph);
 		}
+
+		public Builder phoneNumber(PhoneNumber number) {
+		    this.phoneNumbers.add(number);
+		    return this;
+        }
 		
 		public Builder specialNeeds(String specialNeeds) {
 			this.specialNeeds = specialNeeds;
@@ -403,21 +268,71 @@ public class Staff extends Person{
 			this.thumbnail = thumbnail;
 			return this;
 		}
+
+		public Builder emergencyContactId(UUID id) {
+		    this.emerId = id;
+		    return this;
+        }
+
+		public Builder emergencyContactFirstName(String firstName) {
+		    this.emerFirstName = firstName;
+		    return this;
+        }
+
+        public Builder emergencyContactLastName(String lastName) {
+		    this.emerLastName = lastName;
+		    return this;
+        }
+
+        public Builder emergencyContactPhoneNumber(PhoneNumber number) {
+		    this.emerPhoneNumber = number;
+		    return this;
+        }
+
+		public Builder emergencyContact(EmergencyContact contact) {
+		    this.emergencyContact = contact;
+		    return this;
+        }
 		
-		public Builder emergencyContactName(String name) {
-			this.emerContactName = name;
-			return this;
-		}
-		
-		public Builder emergencyContactNumber(String type, String number) {
-			this.emerContactNumber = new PhoneNumber(type, number);
-			return this;
-		}
-		
-		public Staff done() {
+		public Staff create() {
 		    if (id == null) {
 		        id = UUID.randomUUID();
             }
+            Objects.requireNonNull(firstName);
+            Objects.requireNonNull(lastName);
+            Objects.requireNonNull(title);
+            Objects.requireNonNull(club);
+            if (address == null) {
+                Objects.requireNonNull(streetAddress);
+                Objects.requireNonNull(city);
+                Objects.requireNonNull(state);
+                Objects.requireNonNull(zip);
+                address = Address.builder()
+                        .streetAddress(streetAddress)
+                        .city(city)
+                        .state(state)
+                        .zipcode(zip)
+                        .create();
+            }
+            if (phoneNumbers.isEmpty()) {
+                throw new IllegalArgumentException("You must have at least 1 phone number");
+            }
+            if (emergencyContact == null) {
+                Objects.requireNonNull(emerFirstName);
+                Objects.requireNonNull(emerLastName);
+                Objects.requireNonNull(emerPhoneNumber);
+                if (emerId == null) {
+                    emerId = UUID.randomUUID();
+                }
+                emergencyContact = EmergencyContact.builder()
+                        .id(emerId)
+                        .firstName(emerFirstName)
+                        .lastName(emerLastName)
+                        .phoneNumber(emerPhoneNumber)
+                        .type(Type.CONTACT)
+                        .create();
+            }
+
 			return new Staff(this);
 		}
 	}
