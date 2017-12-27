074779579112ecd2bc95948263b4454081040cfd
John Hameier <jhameier@gmail.com>
2017-12-26 22:17:07 -0500

    Add the uuid.

--- src/main/java/org/vbc4me/awanna/facets/Person.java
+++ src/main/java/org/vbc4me/awanna/facets/Person.java
@@ -1,29 +1,47 @@
 package org.vbc4me.awanna.facets;
 
 import java.util.Objects;
+import java.util.UUID;
 
 public abstract class Person {
 	
-	public enum Type {STAFF, STUDENT, PICKUP}
+	public enum Type {STAFF, STUDENT, PICKUP, CONTACT, GUARDIAN}
+	private final UUID id;
+	private final String first;
+	private final String last;
+	private final Type type;
 	
-	private String first;
-	private String last;
-	private Type type;
-	
-	public Person(String first, String last, Type type) {
-		this.first = Objects.requireNonNull(first);
+	public Person(UUID id, String first, String last, Type type) {
+		this.id = Objects.requireNonNull(id);
+	    this.first = Objects.requireNonNull(first);
 		this.last = Objects.requireNonNull(last);
 		this.type = Objects.requireNonNull(type);
 	}
-	
-	public String firstName() {
+
+    /**
+     * Return the {@link UUID} associated with the {@link Person}.
+     */
+    public UUID id() {
+        return id;
+    }
+
+    /**
+     * Return the first name associated with this {@link Person}.
+     */
+    public String firstName() {
 		return first;
 	}
-	
+
+    /**
+     * Return the last name associated with this {@link Person}.
+     */
 	public String lastName() {
 		return last;
 	}
-	
+
+    /**
+     * Return the type this {@link Person} represents.
+     */
 	public String recordType() {
 		return type.name();
 	}
